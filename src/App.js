0fa8fb915a43b653d3848ab3af45b2ce616369c3
ErwinLambujon <erwinlambujon1@gmail.com>
2024-11-09 02:38:44 +0800

    Going to deploying this application to Vercel

--- src/App.js
+++ src/App.js
@@ -170,6 +170,7 @@ const DigitalSignalEncoding = () => {
             className="border border-gray-300 rounded px-3 py-2 w-full"
           />
         </div>
+        <br />
         {renderSignals()}
       </CardContent>
     </Card>
